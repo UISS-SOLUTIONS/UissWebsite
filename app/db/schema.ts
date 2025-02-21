5f35f8b712e76314e370112822010846d6103563
heismoody <moodyamin531@gmail.com>
2025-02-21 07:08:52 +0300

    created no reltionship tables

--- app/db/schema.ts
+++ app/db/schema.ts
@@ -1,8 +1,28 @@
-import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+import { integer, pgTable, text, varchar } from "drizzle-orm/pg-core";
 
-export const usersTable = pgTable("users", {
+export const sponsors = pgTable("sponsors", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  logo: text(),
   name: varchar({ length: 255 }).notNull(),
-  age: integer().notNull(),
-  email: varchar({ length: 255 }).notNull().unique(),
+});
+
+export const awardsAndAchievements = pgTable("awards_and_achievements", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  title: varchar({ length: 255 }).notNull(),
+  description: text(),
+  date: varchar({length: 255}).notNull()
+});
+
+export const coreValues = pgTable("core_values", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(), 
+  value: varchar({ length: 255 }).notNull(),
+  description: text()
+});
+
+export const heroPage = pgTable("hero_page", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  section: varchar({ length: 255 }).notNull(),
+  heading: varchar({ length: 255 }).notNull(),
+  description: text(),
+  backgroundImg: text()
 });
