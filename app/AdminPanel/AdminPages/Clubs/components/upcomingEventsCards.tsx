0015aeac874a8b11e2277000a7a69e31d7f38b87
heismoody <moodyamin531@gmail.com>
2025-03-09 11:03:24 +0300

    modified pages and created upcoming eventCard

--- app/AdminPanel/AdminPages/Clubs/components/upcomingEventsCards.tsx
+++ app/AdminPanel/AdminPages/Clubs/components/upcomingEventsCards.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const UpcomingEventsCard = () => {
   return (
-    <div className="flex py-4 gap-3 w-full border-b-[1px] border-black/20">
+    <div className="flex py-5 gap-3 w-full border-b-[1px] border-black/20">
       <div className="w-[25%] rounded-md overflow-hidden">
         <img
           src="https://media.istockphoto.com/id/2012746941/photo/cyber-security-black-man-and-code-reflection-in-eyewear-hacking-and-software-update-in-office.webp?a=1&b=1&s=612x612&w=0&k=20&c=QLjdGEHRLLbG5r5hlqd6XCRAflA7D5p61-kOydmcc04="
@@ -10,16 +10,12 @@ const UpcomingEventsCard = () => {
           className="w-full h-full object-cover"
         />
       </div>
-      <div className=" w-[75%] flex flex-col gap-2">
-        <span className="font-bold text-xl">
+      <div className=" w-[75%] flex flex-col gap-1">
+        <span className="font-bold text-lg">
           Catch The Flag 2nd Runner up Winner
         </span>
-        <p className="text-black/60 line-clamp-2">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius
-          mollitia doloribus exercitationem eligendi explicabo laudantium
-          repellat quibusdam labore esse neque.
-        </p>
-        <div className="flex justify-around">
+        
+        <div className="flex flex-col justify-around">
           <div className="flex gap-2 items-center">
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -27,7 +23,7 @@ const UpcomingEventsCard = () => {
               viewBox="0 0 24 24"
               strokeWidth={1.5}
               stroke="currentColor"
-              className="size-6"
+              className="size-5 opacity-60"
             >
               <path
                 strokeLinecap="round"
@@ -40,7 +36,7 @@ const UpcomingEventsCard = () => {
                 d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z"
               />
             </svg>
-            <span className="font-bold">Location Address</span>
+            <span className="text-black/60 font-semibold">Location Address</span>
           </div>
           <div className="flex gap-2 items-center">
             <svg
@@ -49,7 +45,7 @@ const UpcomingEventsCard = () => {
               viewBox="0 0 24 24"
               strokeWidth={1.5}
               stroke="currentColor"
-              className="size-6"
+              className="size-5 opacity-60"
             >
               <path
                 strokeLinecap="round"
@@ -57,7 +53,7 @@ const UpcomingEventsCard = () => {
                 d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
               />
             </svg>
-            <span className="font-bold">2025-04-20 : 14:00</span>
+            <span className="text-black/60 font-semibold">2025-04-20 : 14:00</span>
           </div>
         </div>
       </div>
