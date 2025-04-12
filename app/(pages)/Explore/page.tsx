097428e051cc66d98fd3b1f165c88ff6239bb0ab
heismoody <moodyamin531@gmail.com>
2025-04-12 20:40:13 +0300

    for 12

--- app/(pages)/Explore/page.tsx
+++ app/(pages)/Explore/page.tsx
@@ -1,11 +1,14 @@
+import { fetchData } from "@/app/actions"
 import Quote from "../Home/Quote/Quote"
 import AwardAndArchivement from "./Award&Archivements"
 import CoreValues from "./CoreValues"
 import Governance from "./Governance"
 import VisionAndMission from "./Vision&Mission"
 import WelcomeNote from "./WelcomeNote"
+import { IVisionCard } from "../types"
 
-const Explore = () => {
+const Explore = async () => {
+  const { data } = await fetchData<IVisionCard>(`${process.env.NEXT_PUBLIC_API_ROUTE}/visionMission/3`)
   return (
     <div>
         <WelcomeNote/>
