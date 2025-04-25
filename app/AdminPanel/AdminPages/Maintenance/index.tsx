1bb178ae615b38515591eea1fd278c8702a723b4
heismoody <moodyamin531@gmail.com>
2025-04-25 10:39:11 +0300

    clearing console logs

--- app/AdminPanel/AdminPages/Maintenance/index.tsx
+++ app/AdminPanel/AdminPages/Maintenance/index.tsx
@@ -1,11 +1,8 @@
 import React from "react";
 import HomePageForm from "../../Components/homePageForm";
 import ExplorePageForm from "../../Components/explorePageForm";
-import { auth } from "@/auth";
 
-const WebMaintenance = async () => {
-  const session = await auth()
-  console.log("This is the auth session", session)
+const WebMaintenance = () => {
   return (
     <div className="my-[3vh]">
       <div className="bg-[#FAFAFA] rounded-md shadow-lg mx-10 py-3 px-8 text-3xl font-bold mb-7">
